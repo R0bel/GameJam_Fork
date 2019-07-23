4df7824bd28b80c5e9b7d216b68331abefdd84a8
R <@>
2019-07-23 16:58:25 +0200

    added hooman movement without working rootmotion

---
+++ Assets/Scripts/Hooman/HoomanAnimForward.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoomanAnimForward : MonoBehaviour
+{
+    private AgentAnimMove parentAgentAnimMove;
+
+    private void Awake()
+    {
+        parentAgentAnimMove = transform.parent.GetComponent<AgentAnimMove>();
+        if (parentAgentAnimMove == null) Debug.LogError("Parent has no AgentAnimMove controller attached!");
+    }
+
+    public void OnAnimatorMove()
+    {
+        if (parentAgentAnimMove != null) parentAgentAnimMove.OnAnimatorMove();
+    }
+}
