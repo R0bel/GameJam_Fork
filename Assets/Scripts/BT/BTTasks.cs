9bfc4ea9a5d94b5bb281d66ac1340cb7e20a2ae3
R <@>
2019-07-24 15:29:20 +0200

    BT Test Script

---
+++ Assets/Scripts/BT/BTTasks.cs
@@ -0,0 +1,37 @@
+﻿using Panda;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BTTasks : MonoBehaviour
+{
+    [SerializeField]
+    Animator anim;
+    [SerializeField]
+    NavMeshAgent agent;
+
+    void Awake()
+    {
+        anim = transform.GetComponentInChildren<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        // Don’t update position automatically
+        agent.updatePosition = false;
+    }
+
+
+    [Task]
+    void ShouldMove()
+    {
+        Task task = Task.current;
+        bool shouldMove = agent.remainingDistance > agent.radius - agent.stoppingDistance;
+        if (shouldMove)
+        {
+            task.Succeed();
+        }
+        else
+        {
+            task.Fail();
+        }
+    }
+}
