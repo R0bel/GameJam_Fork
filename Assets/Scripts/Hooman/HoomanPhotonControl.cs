43d968365f3f8078f21f5ed60a7d439c3503975c
MrDigiii <strandlaefer@googlemail.com>
2019-07-25 15:42:22 +0200

    try to fix health issue

--- Assets/Scripts/Hooman/HoomanPhotonControl.cs
+++ Assets/Scripts/Hooman/HoomanPhotonControl.cs
@@ -29,6 +29,9 @@ public class HoomanPhotonControl : MonoBehaviourPun, IPunObservable, IPunInstant
     public void Awake()
     {
         m_PhotonView = GetComponent<PhotonView>();
+        hoomanControl = GetComponent<BTTasks>();
+
+        health = 5;
 
         m_StoredPosition = transform.localPosition;
         m_NetworkPosition = Vector3.zero;
@@ -38,8 +41,7 @@ public class HoomanPhotonControl : MonoBehaviourPun, IPunObservable, IPunInstant
 
     void OnEnable()
     {
-        gameManager = GameManager.Instance;
-        hoomanControl = GetComponent<BTTasks>();
+        gameManager = GameManager.Instance;        
         m_firstTake = true;
     }
 
